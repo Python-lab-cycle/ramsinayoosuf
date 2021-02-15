d585addf59fe2b7eb4347b892c24ed850ec25cec
ramsinarasheed <76723769+ramsinarasheed@users.noreply.github.com>
2021-02-15 20:55:28 +0530

    Add files via upload

---
+++ CO2.4.four digit perfect square with even digit.py
@@ -0,0 +1,11 @@
+#4 digit perfect square with even digit
+import math
+for i in range(1000,10000):
+    num=int(math.sqrt(i))
+    if(num*num==i):
+        n=i;
+        while n!=0:
+            r=n%10
+            n=n//10
+            if r%2!=0:break
+            else:print(i)

55c21c42582446d1eec32f6fc6a863171e039e9c
ramsinarasheed <76723769+ramsinarasheed@users.noreply.github.com>
2021-02-15 20:38:22 +0530

    Add files via upload

---
+++ CO1.4.count the occurence of each word.py
@@ -0,0 +1,12 @@
+#count occurance of each word
+line=input("enter the line:")
+counts={}
+sentence=line.split()
+for word in sentence:
+    if word in counts:
+        counts[word]+=1
+    else:
+        counts[word]=1
+for k,v in counts.items():
+    print(k,v)
+            
